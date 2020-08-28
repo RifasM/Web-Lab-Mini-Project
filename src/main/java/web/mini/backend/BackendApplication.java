7e30bc23d5bee66a2bf1fb7eabdd2f4ee4fd8251
Mohammed Rifas S <rifas2475@outlook.com>
2020-08-28 21:28:38 +0530

    Adding POST
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- src/main/java/web/mini/backend/BackendApplication.java
+++ src/main/java/web/mini/backend/BackendApplication.java
@@ -4,15 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 
 @SpringBootApplication
@@ -22,29 +13,5 @@ public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
-    }
-}
-
-@EnableWebSecurity
-@Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
-                .antMatchers("/api/v1/**").permitAll();
-
-    }
-}
-
-
-@Component
-class SystemLoggedInUserAuditorAware implements AuditorAware<String> {
-
-    @Override
-    public Optional<String> getCurrentAuditor() {
-        return Optional.of(System.getProperty("user.name"));
     }
 }
