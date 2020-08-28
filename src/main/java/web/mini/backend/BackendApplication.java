8a5eef623c6da1b638bc28addc3b745208a89529
Mohammed Rifas S <rifas2475@outlook.com>
2020-08-28 20:02:54 +0530

    Adding CORS
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- src/main/java/web/mini/backend/BackendApplication.java
+++ src/main/java/web/mini/backend/BackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+
 @SpringBootApplication
 @EnableConfigurationProperties
 @EntityScan(basePackages = {"web.mini.backend.model"})  // scan JPA entities
@@ -13,20 +14,4 @@ public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-
-
-    // TODO: CORS HEADER for POST in posts
-    // TODO: CORS HEADER for POST in users
-    // TODO: CORS HEADER for POST in CrossOrigin
-
-    /*
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/v1/posts").allowedOrigins("http://localhost:8080");
-            }
-        };
-    }*/
 }
