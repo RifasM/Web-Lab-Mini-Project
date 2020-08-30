683dd939f7904c06d95214879773fee87c5f2ef4
Mohammed Rifas S <rifas2475@outlook.com>
2020-08-30 20:22:45 +0530

    Mapping
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- src/main/java/web/mini/backend/repository/PostRepository.java
+++ src/main/java/web/mini/backend/repository/PostRepository.java
@@ -1,9 +1,15 @@
 package web.mini.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import web.mini.backend.model.Post;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    @Query("select p from Post p where " +
+            "p.postUser = ?1")
+    List<Post> findAllByUserId(long userID);
 }
