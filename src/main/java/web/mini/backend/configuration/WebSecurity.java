99bcaafc29046a61ca1656f20b56d1e072c407bb
Mohammed Rifas S <rifas2475@outlook.com>
2020-09-13 13:40:16 +0530

    Spring Security In Memory Authentication
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- src/main/java/web/mini/backend/configuration/WebSecurity.java
+++ src/main/java/web/mini/backend/configuration/WebSecurity.java
@@ -6,18 +6,19 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.access.AccessDeniedHandler;
+import web.mini.backend.exception.CustomAccessDeniedHandler;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private AccessDeniedHandler accessDeniedHandler;
+    private CustomAccessDeniedHandler accessDeniedHandler;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        // Disable Cross Site Scripting
+        // TODO: Enable CSRF
         http.csrf().disable();
 
         // The pages does not require login
@@ -33,7 +34,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         // When the user has logged in as XX.
         // But access a page that requires role YY,
         // AccessDeniedException will be thrown.
-        http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
+        //http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
 
         // Config for Login Form
         http.authorizeRequests().and().formLogin()//
@@ -41,25 +42,22 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 //.loginProcessingUrl("/auth-login") // Submit URL
                 .loginPage("/login")//
                 //.defaultSuccessUrl("/home")//
-                //.failureUrl("/login?error=true")//
-                //.usernameParameter("userName")//
+                .failureUrl("/login?error=true")//
+                //.usernameParameter("username")//
                 //.passwordParameter("password")//
                 .permitAll()
                 // Config for Logout Page
                 .and().logout().logoutUrl("/logout").logoutSuccessUrl("/")
                 .and().exceptionHandling().accessDeniedHandler(accessDeniedHandler);
-
-        // Disable Cross Site Scripting
-        // TODO: Enable CSRF
-        http.csrf().disable();
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-
+        // raw = password
+        // hash = $2a$04$gjESNyb94FwBhFte44hls.uEflDlH7Rk1tHzt3uXCnLJm4yjDq/FO
         auth.inMemoryAuthentication()
-                .withUser("user@pixies.org").password("password").roles("USER")
+                .withUser("user@pixies.org").password("$2a$04$gjESNyb94FwBhFte44hls.uEflDlH7Rk1tHzt3uXCnLJm4yjDq/FO").roles("USER")
                 .and()
-                .withUser("admin@pixies.org").password("password").roles("ADMIN");
+                .withUser("admin@pixies.org").password("$2a$04$gjESNyb94FwBhFte44hls.uEflDlH7Rk1tHzt3uXCnLJm4yjDq/FO").roles("ADMIN");
     }
 }
