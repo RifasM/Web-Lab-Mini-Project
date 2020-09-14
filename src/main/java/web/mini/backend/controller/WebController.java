2efb086eedd4927f2d39b4fd5272ba0ad705a4e2
Mohammed Rifas S <rifas2475@outlook.com>
2020-09-14 18:23:08 +0530

    Template Change for Login page
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- src/main/java/web/mini/backend/controller/WebController.java
+++ src/main/java/web/mini/backend/controller/WebController.java
@@ -59,4 +59,9 @@ public class WebController {
     public String authLogin() {
         return "home";
     }
+
+    @RequestMapping("/blog")
+    public String blog(){
+        return "blog";
+    }
 }
