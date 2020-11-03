efee067c79f0bd57b5a0b118bbd1fa1faf3f4adc
Mohammed Rifas S <rifas2475@outlook.com>
2020-11-03 19:43:59 +0530

    view Board Controller
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- src/main/java/web/mini/backend/controller/BoardWebController.java
+++ src/main/java/web/mini/backend/controller/BoardWebController.java
@@ -2,20 +2,33 @@ package web.mini.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import web.mini.backend.exception.ResourceNotFoundException;
 import web.mini.backend.model.Board;
+import web.mini.backend.model.Post;
+import web.mini.backend.repository.BoardRepository;
 import web.mini.backend.repository.PostRepository;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class BoardWebController {
     @Autowired
     private PostRepository postRepository;
+
+    @Autowired
+    private BoardRepository boardRepository;
 
     @Autowired
     private PostController postController;
@@ -64,5 +77,40 @@ public class BoardWebController {
             return "boardTemplates/createBoard";
         else
             return "error";
+    }
+
+    /**
+     * Return Posts under a given Board
+     * Get Mapping
+     *
+     * @param board_id the board id of which posts have to be fetched
+     * @return rendered viewBoard.html
+     */
+    @GetMapping("/viewBoard/{board_id}")
+    public String createPostPage(@PathVariable(value = "board_id") String board_id, Model model)
+            throws ResourceNotFoundException {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        ResponseEntity<Board> board = boardController.getBoardById(board_id);
+
+        // Check if board is private and requesting User is not the board owner, then raise error
+        if (Objects.requireNonNull(board.getBody()).getPrivateBoard() && !auth.getName().equals(board.getBody().getUserId())) {
+            return "error";
+        }
+
+        List<Post> post_list = new ArrayList<>();
+        for (String post_id : board.getBody().getPostID()) {
+            ResponseEntity<Post> result = postController.getPostsById(post_id);
+            if (result.getStatusCode().is2xxSuccessful())
+                post_list.add(result.getBody());
+            else
+                board.getBody().getPostID().remove(post_id);
+        }
+        boardRepository.save(board.getBody());
+
+        model.addAttribute("posts", post_list);
+        model.addAttribute("board_data", board.getBody());
+
+
+        return "boardTemplates/viewBoard";
     }
 }
