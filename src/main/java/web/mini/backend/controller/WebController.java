fc7ec5001fb8b76eb8509e3ee10fbea73824f0aa
Mohammed Rifas S <rifas2475@outlook.com>
2020-11-07 19:51:50 +0530

    Profile Page added
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- src/main/java/web/mini/backend/controller/WebController.java
+++ src/main/java/web/mini/backend/controller/WebController.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 import web.mini.backend.model.Post;
 import web.mini.backend.model.User;
 import web.mini.backend.repository.PostRepository;
@@ -16,7 +15,6 @@ import web.mini.backend.repository.UserRepository;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @Controller
 public class WebController {
@@ -119,56 +117,5 @@ public class WebController {
         Iterable<Post> posts = postController.getAllPosts();
         model.addAttribute("posts", posts);
         return "home";
-    }
-
-    @RequestMapping("/profile")
-    public ModelAndView profile(Authentication auth) {
-        ModelAndView user_data = new ModelAndView("profile");
-        user_data.addObject("username_exists", "no");
-        user_data.addObject("basic_details", userRepository.findByUsername(auth.getName()));
-        return user_data;
-    }
-
-    /**
-     * Update Profile Page
-     *
-     * @return updated profile page
-     */
-    @PostMapping("/profile")
-    public String profile(String id,
-                          String username,
-                          String email,
-                          String first_name,
-                          String last_name,
-                          String phone,
-                          String dob,
-                          String gender,
-                          Model errors) throws ParseException {
-
-        User user = userRepository.findById(Long.parseLong(id)).orElseThrow();
-        User check = userRepository.findByUsername(username);
-
-        if (check != null && !user.getUserName().equals(username)) {
-            errors.addAttribute("username_exists", "yes");
-            return "profile";
-        }
-
-        user.setUserName(username);
-        user.setEmail(email);
-        user.setFirstName(first_name);
-        user.setLastName(last_name);
-        user.setPhone(phone);
-        user.setDob(new SimpleDateFormat("yyyy-MM-dd").parse(dob));
-        user.setGender(gender);
-        user.setUpdatedAt(new Date());
-        user.setUpdatedBy(email);
-
-        userRepository.save(user);
-
-        errors.addAttribute("basic_details", user);
-        errors.addAttribute("username_exists", "no");
-
-        return "redirect:/profile";
-
     }
 }
