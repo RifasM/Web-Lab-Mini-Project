56a37f5e7f322835d297877e3eb362c210487ffe
Mohammed Rifas S <rifas2475@outlook.com>
2020-12-10 11:26:53 +0530

    Disabled Users view
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- src/main/java/web/mini/backend/repository/UserRepository.java
+++ src/main/java/web/mini/backend/repository/UserRepository.java
@@ -5,9 +5,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import web.mini.backend.model.User;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select u from User " +
             "u where u.userName = ?1")
     User findByUsername(String username);
+
+    List<User> findByEnabled(int enabled);
 }
